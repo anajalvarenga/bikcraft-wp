52d494e99a798f7da86fd50457c330e56d2a9cfa
Ana Julia <alvarengaanajulia6@gmail.com>
2021-04-04 15:39:50 -0300

    Criando redes-sociais e adicionando custom fields em page-contato

--- page-contato.php
+++ page-contato.php
@@ -9,7 +9,7 @@
 		<?php include(TEMPLATEPATH . "/inc/introducao.php"); ?>
 
 		<section class="contato container animar-interno">
-			<form action="enviar.php" method="post" name="form" class="formphp contato_form grid-8">
+			<form action="<?php echo get_template_directory_uri(); ?>/enviar.php" method="post" name="form" class="formphp contato_form grid-8">
 				<label for="nome">Nome</label>
 				<input id="nome" name="nome" type="text">
 				<label for="email">E-mail</label>
@@ -30,21 +30,18 @@
 
 			<div class="contato_dados grid-8">
 				<h3>Dados</h3>
-				<span>+55 21 9999-9999</span>
-				<span>orcamento@bikcraft.com</span>
-				<span>Rua Ali Perto - Botafogo</span>
-				<span>Rio de Janeiro - RJ - Brasil</span>
+				<span><?php the_field('telefone'); ?></span>
+				<span><?php the_field('email'); ?></span>
+				<span><?php the_field('endereco_1'); ?></span>
+				<span><?php the_field('endereco_2'); ?></span>
+				<span><?php the_field('telefone'); ?></span>
 				<h3>Redes Sociais</h3>
-				<ul>
-					<li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.png" alt="Facebook Bikcraft"></a></li>
-					<li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.png" alt="Instagram Bikcraft"></a></li>
-					<li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.png" alt="Twitter Bikcraft"></a></li>
-				</ul>
+				<?php include(TEMPLATEPATH . "/inc/redes-sociais.php"); ?>
 			</div>
 		</section>
 
 		<section class="container contato_mapa">
-			<a href="http://google.com" target="_blank" class="grid-16"><img src="img/endereco-bikcraft.jpg" alt="Endereço da Bikcraft"></a>
+			<a href="<?php thefield('link_mapa'); ?>" target="_blank" class="grid-16"><img src="<?php thefield('imagem_mapa'); ?>" alt="<?php thefield('texto_mapa'); ?>"></a>
 		</section>
 
 <?php endwhile; else: endif; ?>
