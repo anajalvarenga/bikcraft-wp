f9125f8cc30da9830a7a02a031328a2f79bbe90c
Ana Julia <alvarengaanajulia6@gmail.com>
2021-04-05 16:51:36 -0300

    Registrando Scripts e CSS

--- functions.php
+++ functions.php
@@ -1,5 +1,31 @@
 <?php 
 
+// Função para registrar os Scripts e CSS
+function bikcraft_scripts(){
+	// Desregistra o jQuery do Wordpress
+	wp_deregister_script('jquery');
+
+	// Registra o jQuery Novo
+	wp_register_script( 'jquery', get_template_directory_uri() . '/js/libs/jquery-1.11.2.min.js', array(), "1.11.2", true );
+
+	// Registra o Script de Plugins, com dependência do jquery, sem especificar versão e no footer do site
+	wp_register_script( 'plugins-script', get_template_directory_uri() . '/js/plugins.js', array( 'jquery' ), false, true );
+
+	// Registra o Script Principal, com dependência do jquery e plugins, sem especificar versão e no footer do site
+	wp_register_script( 'main-script', get_template_directory_uri() . '/js/main.js', array( 'jquery', 'plugins-script' ), false, true );
+
+	// Coloca script no site
+	wp_enqueue_script( 'main-script' );
+}
+add_action('wp_enqueue_scripts', 'bikcraft_scripts');
+
+function bikcraft_css(){
+	wp_register_style( 'bikcraft_style', get_template_directory_uri() . '/style.css', [], false, false );
+
+	wp_enqueue_style( 'bikcraft_style' );
+}
+add_action('wp_enqueue_scripts', 'bikcraft_css');
+
 // Funções para Limpar o Header
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
