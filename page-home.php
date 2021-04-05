969116ea50a29d0a6815f76d1278500c9cca3e52
Ana Julia <alvarengaanajulia6@gmail.com>
2021-04-05 20:54:55 -0300

    Alterando configurações de formulário

--- page-home.php
+++ page-home.php
@@ -32,7 +32,7 @@
 					<p><?php the_field('quote_introducao'); ?></p>
 					<cite><?php the_field('citacao_introducao'); ?></cite>
 				</blockquote>
-				<a href="/sites/bikcraft/produtos/" class="btn">Orçamento</a>
+				<a href="<?php bloginfo('url'); ?>/produtos/" class="btn">Orçamento</a>
 			</div>
 		</section>
 		
@@ -63,7 +63,7 @@
 
 			<div class="call">
 				<p><?php the_field('chamada_produtos'); ?></p>
-				<a href="/sites/bikcraft/produtos" class="btn btn-preto">Produtos</a>
+				<a href="<?php bloginfo('url'); ?>/produtos" class="btn btn-preto">Produtos</a>
 			</div>
 
 		</section>
